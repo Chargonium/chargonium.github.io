a3c01379af350a5d7a15489571942f149dc54ee4
Chargonium <svenvandoorn@icloud.com>
2025-01-16 12:36:47 +0100

    Added admin page + added renovation count

---
+++ public_html/admin/script.js
@@ -0,0 +1,15 @@
+async function login(username, password) {
+    let resp = await fetch("https://api.chargonium.com/auth", {
+        headers: {
+            User: username,
+            Auth: password,
+        },
+    });
+    let token = await resp.text();
+    document.cookie = `token=${token}; Max-Age=86400; path=/`;
+    document.location = "/admin/dashboard";
+}
+
+if (document.cookie.includes("token")) {
+    document.location = "/admin/dashboard";
+}
